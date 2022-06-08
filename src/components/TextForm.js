3f189c0d72bdd242783bd75f00d06a05e2c6aac8
misha <48434217+sandip2118@users.noreply.github.com>
2022-06-08 17:46:18 +0530

    2 commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,6 +18,19 @@ export default function TextForm() {
     setText(newText);
   };
 
+  const handleCopy = () => {
+    var text = document.getElementById('myBox');
+    text.select()
+    navigator.clipboard.writeText(text.value)
+  };
+
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
@@ -48,6 +61,12 @@ export default function TextForm() {
         </button>
         <button onClick={handleClearTextClick} className="btn btn-primary mx-1">
           Clear Text
+        </button>
+        <button onClick={handleCopy} className="btn btn-primary mx-1">
+          Copy
+        </button>
+        <button onClick={handleExtraSpaces} className="btn btn-primary mx-1">
+          Remove Extra Spaces
         </button>
       </div>
       <div class="container m-4"></div>
